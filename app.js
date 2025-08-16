f2842a3a3aa175b86f54a781b499c4fc6bf2a24c
samueldob <frankdobisamuel@gmail.com>
2025-08-16 19:34:03 +0100

    add: installed cors

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const mailerRoutes = require("./routes/mailerRoutes")
+const cors = require("cors");
 
+app.use(cors({
+    origin: ["http://localhost:5173", "https://your-netlify-app.netlify.app"], 
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+  }));
 
 dotenv.config();
 
