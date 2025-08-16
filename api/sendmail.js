49a7415738a2aa39e4608e878e40aa383c6db154
samueldob <frankdobisamuel@gmail.com>
2025-08-16 23:22:21 +0100

    test: testing cors

--- api/sendmail.js
+++ api/sendmail.js
@@ -10,6 +10,9 @@ module.exports = async (req, res) => {
       "Access-Control-Allow-Headers",
       "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
     );
+    if (req.method === "OPTIONS") {
+        return res.status(200).end();
+      }
   if (req.method !== 'POST') {
     return res.status(405).json({ success: false, message: 'Method not allowed' });
   }
